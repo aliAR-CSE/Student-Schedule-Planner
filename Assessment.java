8553b3daf4e5ebc4b8e8cbe92d656f5a9a113819
aliAR <aliabdulrazak@gmail.com>
2026-06-02 16:24:50 -0400

    Assessment constructor

--- Assessment.java
+++ Assessment.java
@@ -1,9 +1,23 @@
 import java.time.LocalDateTime;
 
 public class Assessment{
-    private String[] assessmentName;
-    private String[] assessmentType;
+
+    private String assessmentName;
+    private String assessmentType;
     private LocalDateTime dueDate;
+    private int assessmentWeight;
+    private int gradeEarned;
+    private boolean isComplete;
 
-
+    public Assessment(String assessmentName, String assessmentType, LocalDateTime dueDate, int assessmentWeight, int gradeEarned, boolean isComplete){
+        this.assessmentName = assessmentName;
+        this.assessmentType = assessmentType;
+        this.dueDate = dueDate;
+        this.assessmentWeight = assessmentWeight;
+        this.gradeEarned = gradeEarned;
+        this.isComplete = isComplete;
+    }
+    public Assessment(){
+        this("New Assessment", "Assignment", LocalDateTime.now(), 0, 0, false);
+    }
 }
